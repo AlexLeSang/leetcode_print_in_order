eb97d95223b3ea0454334279e0036c372603b9bb
Oleksandr Halushko <oleksandr.halushko@sioux.eu>
2019-10-08 16:30:18 +0200

    Add solution with atomic int

---
+++ main-atomic.cpp
@@ -0,0 +1,93 @@
+#include <cassert>
+#include <condition_variable>
+#include <functional>
+#include <future>
+#include <iostream>
+#include <memory>
+#include <sstream>
+#include <atomic>
+
+using namespace std;
+
+class Foo {
+  atomic_int i = 0;
+
+public:
+  Foo() {}
+
+  void first(function<void()> printFirst) {
+    while (i.load() != 0)
+      ;
+
+    // printFirst() outputs "first". Do not change or remove this line.
+    printFirst();
+
+    i.store(1);
+  }
+
+  void second(function<void()> printSecond) {
+    while (i.load() != 1)
+      ;
+
+    // printSecond() outputs "second". Do not change or remove this line.
+    printSecond();
+
+    i.store(2);
+  }
+
+  void third(function<void()> printThird) {
+    while (i.load() != 2)
+      ;
+
+    // printThird() outputs "third". Do not change or remove this line.
+    printThird();
+
+    i.store(3);
+  }
+};
+
+void test_funciton() {
+  ostringstream ss;
+  mutex ss_mutex;
+
+  auto printFirst = [&ss, &ss_mutex]() {
+    unique_lock l(ss_mutex);
+    ss << "first";
+  };
+  auto printSecond = [&ss, &ss_mutex]() {
+    unique_lock l(ss_mutex);
+    ss << "second";
+  };
+
+  auto printThird = [&ss, &ss_mutex]() {
+    unique_lock l(ss_mutex);
+    ss << "third";
+  };
+
+  Foo foo{};
+
+  const auto first =
+      async(launch::async, [&foo, &printFirst]() { foo.first(printFirst); });
+  const auto second =
+      async(launch::async, [&foo, &printSecond]() { foo.second(printSecond); });
+  const auto third =
+      async(launch::async, [&foo, &printThird]() { foo.third(printThird); });
+
+  first.wait();
+  second.wait();
+  third.wait();
+
+  const auto result = ss.str();
+  const auto cpm_result = string{"firstsecondthird"} == result;
+  if (!cpm_result)
+    cout << result << endl;
+  assert(cpm_result);
+}
+
+int main(int, char **) {
+  for (auto i = 0; i < static_cast<int>(1e5); ++i) {
+    test_funciton();
+  }
+
+  return 0;
+}
